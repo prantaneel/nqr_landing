014e6cafce8c4e0f19264c830c1daade407a6ea6
prantaneel-ti <120361228+prantaneel-ti@users.noreply.github.com>
2023-08-28 09:17:25 +0530

    fixed footer note

--- app/footer.tsx
+++ app/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <div className="width-60 justify-center text-blue-200 margin-top-20p">
       <div className="text-align-center">
-        <a className = "link-100 link-no-underline" href="#">Founder's Notes</a>
+        <a className = "link-100 link-no-underline" href="#">Founder Note</a>
       </div>
     </div>
   );
